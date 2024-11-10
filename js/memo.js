abf0727ea6e0af60e855632de54ddcbf4216a7fc
m15obayasi <m15obayasi@gmail.com>
2024-11-10 17:26:44 +0900

    回答ボタンを作成中

--- js/memo.js
+++
@@ -1,8 +0,0 @@
-
-// 数字を入力すると、次の項目に遷移。
-function moveToNextInput(currentInput) {
-    const nextInput = $(currentInput).next('input');
-    if (nextInput.length) {
-        nextInput.focus();
-    }
-}
