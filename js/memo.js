01b89988ea89bbe0af33c580b306f2fa1a72923f
m15obayasi <m15obayasi@gmail.com>
2024-11-11 23:38:43 +0900

    忘れないうちに

--- js/memo.js
+++ js/memo.js
@@ -1,33 +1,51 @@
 var number = 1; // 問題数
 
-// $(".question").hide();
-// $(".form").hide();
-// $("h2").hide();
+$(".question").hide();
+$(".form").hide();
+$("h2").hide();
 
 // １つ目の要素だけ取り出したい……。
 var questions = [
-    ["AAAA","0"],
-    ["BBBB","0"]
+    ["「ニンテンドーDS」発売", "2004"],
+    ["「ドラえもん」の声優が一新","2005"],
+    ["ソフトバンクがボーダフォンを買収","2006"],
+    ["電子マネー「nanaco」開始","2007"],
+    ["そばにいるね（Ado）","2008"],
+    ["1Q84（村上春樹）発売", "2009"],
+    ["お・も・て・な・し", "2013"],
+    ["STAP細胞はあります！", "2014"],
+    ["国立競技場　ザハ案の廃止","2015"],
+    ["PPAP（ピコ太郎）","2016"],
+    ["旅行会社「てるみくらぶ」が倒産","2017"],
+    ["日大アメフト部の「危険タックル」","2018"],
+    ["消費税が「10％」になる", "2019"],
+    ["PlayStation5　発売", "2020"],
+    ["うっせぇわ（Ado）", "2021"],
+    ["三苫の1mm", "2022"],
+    ["「猫ミーム」がちょっと流行る", "2023"],
 ];
 
 /* スタートを押したときのアクション */
 $(".answer-button").on("click", function () {
-    $(".question").show();
-    $(".form").show();
-    $("h2").show();
+    $(".question, .form, .h2").show();
     $(".answer").html("回答");
-    var random = Math.floor(Math.random()*questions.length);
-    $(".question").html(questions[random]);
-});
 
-/* 入力した「年」を取得 */
-$(".answer-button").on("click", function () {
-    const year1 = $("#y-1").val();
-    const year2 = $("#y-2").val();
-    const year3 = $("#y-3").val();
-    const year4 = $("#y-4").val();
-    const yearAll = year1 + year2 + year3 + year4;
-    console.log(yearAll)
+    /* 問題文を表示 */
+    var random = Math.floor(Math.random() * questions.length);
+    $(".question").html(questions[random][0]);
+    console.log("正解 = " + questions[random][1])
+
+    /* 回答内容を送信 */
+    const ansYear = $("#y-1").val() + $("#y-2").val() + $("#y-3").val() + $("#y-4").val();
+    console.log("回答 = " + ansYear)
+
+    /* 正誤判定 */
+    if (ansYear === questions[random][1]) {
+        console.log("正解")
+    } else {
+        console.log("不正解")
+    }
+
 });
 
 // 問題数を表示
