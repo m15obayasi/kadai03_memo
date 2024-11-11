b63e830844e3b3dd8c585679590e6ce277fb846a
m15obayasi <m15obayasi@gmail.com>
2024-11-12 00:08:33 +0900

    悪くない状況

--- js/memo.js
+++ js/memo.js
@@ -1,8 +1,6 @@
 var number = 1; // 問題数
 
-$(".question").hide();
-$(".form").hide();
-$("h2").hide();
+$(".question, .form, .answer, h2").hide();
 
 // １つ目の要素だけ取り出したい……。
 var questions = [
@@ -10,7 +8,7 @@ var questions = [
     ["「ドラえもん」の声優が一新","2005"],
     ["ソフトバンクがボーダフォンを買収","2006"],
     ["電子マネー「nanaco」開始","2007"],
-    ["そばにいるね（Ado）","2008"],
+    ["そばにいるね（青山テルマ）","2008"],
     ["1Q84（村上春樹）発売", "2009"],
     ["お・も・て・な・し", "2013"],
     ["STAP細胞はあります！", "2014"],
@@ -25,31 +23,41 @@ var questions = [
     ["「猫ミーム」がちょっと流行る", "2023"],
 ];
 
-/* スタートを押したときのアクション */
-$(".answer-button").on("click", function () {
-    $(".question, .form, .h2").show();
-    $(".answer").html("回答");
+/* スタートボタンを押したときのアクション */
+$(".start-button").on("click", function () {
+    $(".question, .form, .answer, h2").show();
+    $(".start-button").hide();
+    $("h2").html("第" + number + "問");
 
     /* 問題文を表示 */
     var random = Math.floor(Math.random() * questions.length);
     $(".question").html(questions[random][0]);
-    console.log("正解 = " + questions[random][1])
+    console.log("問題の正解 = " + questions[random][1])
+
+    /* 回答ボタンを押したときのアクション */
+    $(".answer-button").on("click", function () {
+        $(".question, .form, h2").show();
+        $(".answer").html("回答");
 
     /* 回答内容を送信 */
     const ansYear = $("#y-1").val() + $("#y-2").val() + $("#y-3").val() + $("#y-4").val();
-    console.log("回答 = " + ansYear)
+    console.log("あなたの回答 = " + ansYear)
 
     /* 正誤判定 */
-    if (ansYear === questions[random][1]) {
-        console.log("正解")
-    } else {
+        if (ansYear === questions[random][1]) {
+        console.log("正解！！")
+        console.log("-----------")
+        random = Math.floor(Math.random() * questions.length);
+        $(".question").html(questions[random][0]);
+        console.log("問題の正解 = " + questions[random][1])
+        /* 正解したら次の問題へ…… */
+        number++;
+            $("h2").html("第" + number + "問");
+        } else {
         console.log("不正解")
+        console.log("-----------")
+        console.log("問題の正解 = " + questions[random][1])
     }
+    });
 
-});
-
-// 問題数を表示
-$(".answer-button").click(function () {
-    var noq = number ++;
-    $("h2").html("第" + noq + "問");
 });
